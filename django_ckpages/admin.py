305ffb00faee4f096efd5ee9fe9be49d1c23eb21
Marco Pompili <marcs.pompili@gmail.com>
2014-03-17 19:16:44 +0100

    Code cleaning.

--- django_ckpages/admin.py
+++ django_ckpages/admin.py
@@ -1,8 +1,8 @@
-'''
+"""
 Created on 05/lug/2013
 
 @author: Marco Pompili
-'''
+"""
 
 from django.conf import settings
 from django.db import models
@@ -12,19 +12,21 @@ from django.contrib import admin
 from django.contrib.flatpages.models import FlatPage
 #from django.contrib.flatpages.admin import FlatPageAdmin
 
+
 class CKPageAdmin(admin.ModelAdmin):
     formfield_overrides = {
         models.TextField: {
-            'widget' : forms.Textarea(attrs = { 'class' : 'ckeditor '})
+            'widget': forms.Textarea(attrs={'class': 'ckeditor '})
         }
     }
-    
+
     class Media:
         css = {
-            'all' : ('/static/django_ckpages/css/style.css',)
+            'all': ('/static/django_ckpages/css/style.css',)
         }
         js = ('http://cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/ckeditor.js',
               settings.CK_CONFIGURATION,)
+
 
 admin.site.unregister(FlatPage)
 admin.site.register(FlatPage, CKPageAdmin)
--- django_ckpages/models.py
+++ django_ckpages/models.py
@@ -1,10 +1,11 @@
-'''
+"""
 Created on 05/lug/2013
 
 @author: Marco Pompili
-'''
+"""
 
 from django.contrib.flatpages.models import FlatPage
+
 
 class CKPage(FlatPage):
     pass
